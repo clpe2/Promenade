ed4b90eb327dd5e238098ef344eab27a492c7d9f
Caroline <58362658+wattcaro@users.noreply.github.com>
2021-06-20 12:36:25 +0200

    localizer base

---
+++ Localizer/app/src/main/java/jupiter/com/localizer/MainActivity.kt
@@ -0,0 +1,14 @@
+package jupiter.com.localizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RadioGroup
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
